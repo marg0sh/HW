705b86f5abb2816102395ed81a73c3e245717cd7
Margarita <103373222+marg0sh@users.noreply.github.com>
2022-05-08 12:41:11 +0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+cd p1 
+mkdir p2 p3 p4 
+cd p2 
+touch t1.txt t2.txt t3.txt j1.json j2.json 
+mkdir p5 p6 p7 
+ls -la 
+mv /c/Users/user/QA/p1/p2/{t1.txt,t2.txt} /c/Users/user/QA/p1/p3/
